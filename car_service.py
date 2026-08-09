b5845df920014f10264e802dd6b1e3d5774401a3
Michael Sandrovich <michael231559@gmail.com>
2026-08-09 15:21:05 +0300

    connect user method fixes

--- car_service.py
+++ car_service.py
@@ -12,10 +12,14 @@ def connect_user(shortcut_token):
     active_driver = get_active_driver()
 
     if active_driver:
-        return {
-            "message": "Car is already in use",
-            "current_driver": active_driver[0]
-        }
+        if active_driver[0] == user[1]:
+            return {
+                "message": "User is already the current driver",
+                "current_driver": active_driver[0]
+            }
+
+        # Handover: close previous driver's active session
+        insert_car_event(active_driver[0], "disconnected")
 
     result = insert_car_event(user[1], "connected")
 
